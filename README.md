313a215c7d5cfdabbff5babdeb0ce3bdf7457606
Stoyan Stoyanov <stoyan.stoyanov@weltn24.de>
2015-11-08 19:23:37 +0100

    added some text in README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # simple-logging-pipeline
+
+  vagrant up
+
+## Testing
+
+  run 'chef exec kitchen test' in the cookbook to test every single cookbook 
---
+++ cookbooks/apache2/metadata.rb
@@ -0,0 +1,7 @@
+name 'apache2'
+maintainer 'Stoyan Stoyanov'
+maintainer_email 'stoyanoff.s@gmail.com'
+license 'all_rights'
+description 'Installs/Configures apache2'
+long_description 'Installs/Configures apache2'
+version '0.1.0'
