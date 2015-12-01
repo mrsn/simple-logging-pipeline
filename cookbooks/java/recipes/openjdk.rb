41c38bb825cf8676fb14132f18469f8e8ed45c5a
Stoyan Stoyanov <stoyan.stoyanov@weltn24.de>
2015-12-01 11:00:21 +0100

    not pinning jdk version anymore

--- cookbooks/java/recipes/openjdk.rb
+++ cookbooks/java/recipes/openjdk.rb
@@ -1,6 +1,5 @@
 execute 'apt-get update'
 
 package 'openjdk-7-jdk' do
-  version '7u85-2.6.1-5ubuntu0.14.04.1'
   action :install
 end
--- cookbooks/java/test/integration/java/serverspec/openjdk_spec.rb
+++ cookbooks/java/test/integration/java/serverspec/openjdk_spec.rb
@@ -9,11 +9,3 @@ set :backend, :exec
     it { expect(package(package_name)).to be_installed }
   end
 end
-
-describe 'java version' do
-  it { expect(java_version).to include('1.7.0_85') }
-end
-
-def java_version
-  command('java -version').stdout
-end
